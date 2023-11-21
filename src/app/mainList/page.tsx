a258b41366d1e2905bd7aa2e9f9b975a12c787b1
junyeoplee <jyeop920@gmail.com>
2023-11-21 17:17:05 +0900

    Refactor : Mock api 에서 실제 youtube api 로 리팩토링
    ###
    - [x] Mock api 에서 실제 youtube api 로 리팩토링
    
    ### 반영 브랜치
    refactor/mainList -> develop
    
    ### 변경 사항
    Mock api 방식에서 axios 로 get 메소드를 사용 하여 실제 youtube api 로 요청하여 데이터를 가져오도록 리팩토링 했습니다

--- src/app/mainList/page.tsx
+++ src/app/mainList/page.tsx
@@ -1,15 +1,19 @@
 import Link from 'next/link'
-import { NextPage } from 'next';
+import { NextPage } from 'next'
 import styles from '@/app/mainList/page.module.scss'
 import React from 'react'
 import VIDEO_LIST from '@public/videos/popular.json'
 import formatRelativeDate from '@/utils/relativeDate'
 import axios from 'axios'
-import { IVideo,ISnippet  } from '@/type/Api';
+import { IVideo, ISnippet } from '@/type/Api'
 
 type VideoList = IVideo[]
 
-const VideoListPage:NextPage<ISnippet> = (): React.JSX.Element => {
+const VideoListPage: NextPage<ISnippet> = (): React.JSX.Element => {
+  //const ACCESS_KEY = process.env.YOUTUBE_API_KEY
+  //const URL = https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${ACCESS_KEY}
+  //const response = await (await axios.get(URL)).data
+  //const videoList: VideoList = response.items
   const videoList: VideoList = VIDEO_LIST.items
   console.log(videoList)
   return (
@@ -19,14 +23,14 @@ const VideoListPage:NextPage<ISnippet> = (): React.JSX.Element => {
           const VIDEO = video.snippet
           return (
             <li className={styles.videoCard}>
-              <Link 
-                className={styles.videoLink} 
-                href={
-                  {pathname:`/detail/${VIDEO.channelId}`,
-                   query:{videoInfo: encodeURIComponent(JSON.stringify(VIDEO))}
-                   }
-                }
-                // as={`/detail/${VIDEO.channelId}`}
+              <Link
+                className={styles.videoLink}
+                href={{
+                  pathname: `/detail/${VIDEO.channelId}`,
+                  query: {
+                    videoInfo: encodeURIComponent(JSON.stringify(VIDEO)),
+                  },
+                }}
               >
                 <div>
                   <img
