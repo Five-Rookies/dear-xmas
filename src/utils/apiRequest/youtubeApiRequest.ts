f49f823330b9cd971c176ca2204d01d80a09c47f
zxxng <jeayeong123@naver.com>
2023-11-23 13:53:46 +0900

    feat: youtube api 통신 utils 함수 추가 #33
    Co-authored-by: Song Su Bin / 송수빈 <ssb1565b@users.noreply.github.com>

--- src/utils/apiRequest/youtubeApiRequest.ts
+++ src/utils/apiRequest/youtubeApiRequest.ts
@@ -1,3 +1,43 @@
-const youtubeApiRequest = () => {}
+import axios from 'axios'
+
+interface IYoutubeApiRequest {
+  apiType: 'videos' | 'search'
+  optionalQuery: string
+  maxResults: number
+}
+
+const youtubeApiRequest = async ({
+  apiType,
+  optionalQuery,
+  maxResults,
+}: IYoutubeApiRequest) => {
+  const ACCESS_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+  const BASIC_URL = `https://youtube.googleapis.com/youtube/v3/${apiType}?part=snippet&regionCode=kr`
+  const URL = `${BASIC_URL}${optionalQuery}&maxResults=${maxResults}&key=${ACCESS_KEY}`
+  let response = ''
+  try {
+    response = await axios.get(URL)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+
+  if (!response) {
+    throw new Error('data is not defined')
+  }
+
+  return response
+}
 
 export default youtubeApiRequest
+
+// [['chart', 'mostPopular'] , ['maxResults','32' ]]
+// `&chart=mostPopular&maxResults=32`
+// `&chart=mostPopular&maxResults=32`
+// &channelId=${channelId}&q=surfing
+// &maxResults=25
+//
+
+// &maxResults=25
+// &q=${search}
+// &type=video
+// &regionCode=kr
