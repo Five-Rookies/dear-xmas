5dcc739158b594661033aa7184a26f4eec72f01c
zxxng <jeayeong123@naver.com>
2023-11-24 10:30:29 +0900

    feat: 댓글 컴포넌트 추가 및 헤더 css 수정 #36
    Co-authored-by: Song Su Bin / 송수빈 <ssb1565b@users.noreply.github.com>

---
+++ src/app/detail/[id]/DetailHeader.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import styles from './detail.module.scss'
+
+const DetailHeader = ({ title }: { title: string }) => {
+  const router = useRouter()
+
+  return (
+    <header className={styles.header}>
+      <button type="button" onClick={() => router.back()}>
+        <img src="/assets/left-arrow.svg" alt="뒤로가기 아이콘" />
+        <span>이전으로</span>
+      </button>
+      <p>{title}</p>
+    </header>
+  )
+}
+
+export default DetailHeader
