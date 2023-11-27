51720150e54120202769c5f1b6e096648d27beda
zxxng <jeayeong123@naver.com>
2023-11-27 10:47:39 +0900

    refactor: mainList -> home 으로 이동 #44

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
+import { NextPage } from 'next'
+import React from 'react'
+import { IVideo, ISnippet } from '@/type/Api'
+import VideoList from '@/components/home/VideoList'
+import youtubeApiRequest from '@/utils/apiRequest/youtubeApiRequest'
+import testJSON from '@public/videos/popular.json'
 import styles from './page.module.scss'
 
-export default function Home() {
+type VideoListType = IVideo[]
+
+const getVideoList = async (): Promise<VideoListType> => {
+  // const response = await youtubeApiRequest()
+  // return response
+  return testJSON.items
+  /* const ACCESS_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+	const URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=32&key=${ACCESS_KEY}`
+	const response = await (await axios.get(URL)).data.items
+	if (!response) {
+		throw new Error('data is not defined')
+	}
+	return response */
+}
+
+const Home = async () => {
+  const videoList: VideoListType = await getVideoList()
+  // const videoList: VideoListType = VIDEO_LIST.items
+
   return (
     <main className="main-container">
       <section className={styles.bg}>
@@ -23,6 +47,11 @@ export default function Home() {
       </section>
 
       <div className="innerBox"></div>
+      <section className="inner-box">
+        <VideoList videoList={videoList} />
+      </section>
     </main>
   )
 }
+
+export default Home
--- src/components/home/VideoList.tsx
+++ src/components/home/VideoList.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useRef } from 'react'
-import styles from '@/app/mainList/page.module.scss'
+import styles from '@/app/page.module.scss'
 import formatRelativeDate from '@/utils/relativeDate'
 import Link from 'next/link'
 import { IVideo } from '@/type/Api'
@@ -11,54 +11,58 @@ import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 type VideoListType = IVideo[]
 
 const VideoList = ({ videoList }: { videoList: VideoListType }) => {
-  //스크롤버튼
-  const sectionList = useRef(null);
-  const videoCardRef = useRef<HTMLLIElement | null>(null);
-  const [isAtBottom, setIsAtBottom] = useState(false);
+  // 스크롤버튼
+  const sectionList = useRef(null)
+  const videoCardRef = useRef<HTMLLIElement | null>(null)
+  const [isAtBottom, setIsAtBottom] = useState(false)
 
-  //무한스크롤
-  const [list, setList] = useState();//영상목록 저장
-  const [searchQ, setSearchQ] = useState();//마지막으로 검색한 단어를 저장, nextPageToken 사용할 때 필요
-  const [nextPageToken, setNextPageToken] = useState();//nextPageToken을 저장
-  const [isLoading, setIsLoading]= useState(false);//로딩중 애니메이션 state
+  // 무한스크롤
+  const [list, setList] = useState() // 영상목록 저장
+  const [searchQ, setSearchQ] = useState() // 마지막으로 검색한 단어를 저장, nextPageToken 사용할 때 필요
+  const [nextPageToken, setNextPageToken] = useState() // nextPageToken을 저장
+  const [isLoading, setIsLoading] = useState(false) // 로딩중 애니메이션 state
 
   const handleScroll = () => {
-    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    //스크롤이 최하단에 있을 때
-    const atBottom = currentScrollTop + window.innerHeight >= document.documentElement.scrollHeight;
-  
-    setIsAtBottom(atBottom);
+    const currentScrollTop =
+      window.pageYOffset || document.documentElement.scrollTop
+    // 스크롤이 최하단에 있을 때
+    const atBottom =
+      currentScrollTop + window.innerHeight >=
+      document.documentElement.scrollHeight
+
+    setIsAtBottom(atBottom)
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   const scrollToSection = () => {
-    const cardHeight = videoCardRef.current?.offsetHeight || 0;
-    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const cardHeight = videoCardRef.current?.offsetHeight || 0
+    const currentScrollTop =
+      window.pageYOffset || document.documentElement.scrollTop
 
-    //이동할 스크롤 위치 계산
-    const newScrollTop = isAtBottom ? 0 : currentScrollTop + cardHeight;
- 
-    //다음 위치로 스크롤
+    // 이동할 스크롤 위치 계산
+    const newScrollTop = isAtBottom ? 0 : currentScrollTop + cardHeight
+
+    // 다음 위치로 스크롤
     window.scrollTo({
       top: newScrollTop,
       behavior: 'smooth',
     })
   }
-  
+
   return (
     <>
       <ul className={styles.videoList} ref={sectionList}>
         {videoList.map((video: IVideo) => {
           const VIDEO = video.snippet
           return (
-            <li 
-              className={`videoCard ${styles.videoCard}`} 
+            <li
+              className={`videoCard ${styles.videoCard}`}
               key={video.id}
               ref={videoCardRef}
             >
@@ -96,14 +100,15 @@ const VideoList = ({ videoList }: { videoList: VideoListType }) => {
           )
         })}
       </ul>
-      <button 
+      <button
         className={`scrollBtn ${styles.scrollBtn}`}
-        onClick={() => scrollToSection()}>
-          <FontAwesomeIcon 
-            className={styles.arrowDownIcon} 
-            icon={isAtBottom ? faArrowUp : faArrowDown}
-          />
-        </button>
+        onClick={() => scrollToSection()}
+      >
+        <FontAwesomeIcon
+          className={styles.arrowDownIcon}
+          icon={isAtBottom ? faArrowUp : faArrowDown}
+        />
+      </button>
     </>
   )
 }
