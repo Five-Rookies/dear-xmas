f4061ef558743d913bef0e9f0702a8ef0d9b07ae
zxxng <jeayeong123@naver.com>
2023-11-29 14:14:59 +0900

    feat: youtube data 요청 커스텀훅 추가 #67

---
+++ src/hooks/useYoutubeApiRequest.ts
@@ -0,0 +1,92 @@
+'use client'
+
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import useStore from '@/status/store'
+import { IYoutubeResponse } from '@/type/Api'
+
+const printErrorMessage = (error: unknown) => {
+  if (error instanceof Error) {
+    console.error(error.message)
+  }
+}
+
+const youtubeApiRequest = async (
+  setQuotaExhausted: () => void,
+  optionalQuery: string = '&q=크리스마스|크리스마스영화',
+  maxResults: number = 32,
+) => {
+  const ACCESS_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+  const baseURL = `https://youtube.googleapis.com/youtube/v3/search?`
+  const commonQuery = `&nextPageToken=CBkQAA&regionCode=kr&type=video&order=relevance&videoSyndicated=true&maxResults=${maxResults}`
+  const URL = `${baseURL}${commonQuery}${optionalQuery}&key=${ACCESS_KEY}`
+
+  try {
+    const youtubeApiData = await axios.get(URL)
+    return youtubeApiData!.data
+  } catch (error) {
+    setQuotaExhausted()
+    console.log('[NOTICE] Youtube API 요청 불가. JSON 데이터로 대체합니다.')
+    printErrorMessage(error)
+  }
+}
+
+const youtubeJsonRequest = async (
+  apiType: string = 'popular',
+  optionalQuery: string = '',
+) => {
+  try {
+    let jsonData
+    if (apiType === 'popular' || apiType === 'search') {
+      jsonData = await import(
+        '@public/videos/christmas/christmasPopular_v1.json'
+      )
+    } else {
+      const channelId = optionalQuery.split('=')[1]
+      jsonData = await import(
+        `@public/videos/christmas/searchByChannels/search-by-channel-id-${channelId}.json`
+      )
+    }
+
+    return jsonData.default
+  } catch (error) {
+    console.log('[NOTICE] 유효하지 않은 JSON 데이터 요청입니다.')
+    printErrorMessage(error)
+  }
+}
+
+const useYoutubeDataRequest = (
+  apiType: string = 'popular',
+  optionalQuery: string = '&q=크리스마스|크리스마스영화',
+  maxResults: number = 32,
+) => {
+  const { isApiQuotaExhausted, setQuotaExhausted } = useStore()
+  const [data, setData] = useState<IYoutubeResponse | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response
+      if (isApiQuotaExhausted) {
+        response = await youtubeJsonRequest(apiType, optionalQuery)
+      } else {
+        try {
+          response = await youtubeApiRequest(
+            setQuotaExhausted,
+            optionalQuery,
+            maxResults,
+          )
+        } catch (error) {
+          response = await youtubeJsonRequest(apiType, optionalQuery)
+        }
+      }
+      setData(response)
+    }
+
+    // 데이터가 없을 경우에만 데이터 패칭
+    if (data === null) fetchData()
+  }, [isApiQuotaExhausted])
+
+  return data
+}
+
+export default useYoutubeDataRequest
