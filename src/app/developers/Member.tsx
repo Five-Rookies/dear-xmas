1c57168f511243a3bf78aca193789bf19857cfe0
ssb1565b <ssongsumovie@naver.com>
2023-11-29 10:14:37 +0900

    feat: 화면 사이즈에 맞게 잔디 보여주기 #66

--- src/app/developers/Member.tsx
+++ src/app/developers/Member.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import styles from './developers.module.scss'
 import { IDeveloper } from '@/type/Component'
 import Link from 'next/link'
+import GitHubCalendar from 'react-github-calendar'
+
+// type
+interface Day {
+  date: string
+  count: number
+  level: 0 | 1 | 2 | 3 | 4
+}
+
+const selectLastHalfYear = (contributions: Day[], shownMonths: number) => {
+  const currentDate = new Date()
+  const currentYear = currentDate.getFullYear()
+  const currentMonth = currentDate.getMonth() + 1
+
+  return contributions.filter((day: Day) => {
+    const date = new Date(day.date)
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+
+    const monthDiff = (currentYear - year) * 12 + (currentMonth - month)
+    return monthDiff < shownMonths
+  })
+}
 
 const Member = ({ member }: { member: IDeveloper }) => {
+  const [displayWidth, setDisplayWidth] = useState(8)
   const githubId = member.github.split('/')
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth
+      if (width <= 768 && width >= 480) {
+        setDisplayWidth(10)
+      } else {
+        setDisplayWidth(8)
+      }
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
   return (
     <div className={styles.memberBox}>
       <div className={styles.memberInfo}>
@@ -22,8 +63,10 @@ const Member = ({ member }: { member: IDeveloper }) => {
         </div>
       </div>
       <div className={styles.githubContainer}>
-        <img
-          src={`https://ghchart.rshah.org/${githubId[githubId.length - 1]}`}
+        <GitHubCalendar
+          username={githubId[githubId.length - 1]}
+          blockSize={12}
+          transformData={day => selectLastHalfYear(day, displayWidth)}
         />
       </div>
     </div>
