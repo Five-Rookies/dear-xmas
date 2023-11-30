088fc941bd51b9ee696dac1705336e649c4fe69e
seonyeongyoon <ssunnyy0204@gmail.com>
2023-11-30 10:45:32 +0900

    no-result 페이지 이미지  Image 컴포넌트로 수정

--- src/app/search/NoResult.tsx
+++ src/app/search/NoResult.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from './search.module.scss'
+import Image from 'next/image'
+
 
 const NoResult = () => {
   return (
@@ -9,7 +11,14 @@ const NoResult = () => {
         <p className={styles.tag}>No Results</p>
         <h1>검색 결과가 없습니다</h1>
         <p className={styles.tryAnotherKeword}>다른 키워드를 입력해 보세요!</p>
-        <img src="/assets/no-results.png" alt="" />
+        <Image
+          src="/assets/no-results.png"
+          width={0}
+          height={0}
+          sizes="25rem"
+          style={{width: '25rem', height: 'auto'}}
+          alt=""
+        />
         <Link href="/">홈으로 돌아가기</Link>
       </div>
     </div>
