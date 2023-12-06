57753ce44c40ee48f849aaa1af2f211746ddd3b9
ssb1565b <ssongsumovie@naver.com>
2023-12-06 17:40:58 +0900

    feat: 채팅 소켓으 구현 #104

--- src/app/(meetUp)/live/page.tsx
+++ src/app/(meetUp)/live/page.tsx
@@ -2,13 +2,25 @@ import React from 'react'
 import LiveStream from './_components/LiveStream'
 import LiveChat from './_components/LiveChat'
 import styles from './live.module.scss'
+import { GetChat } from '@/utils/apiRequest/commentsApiRequest'
+import DetailHeader from '../detail/[id]/_components/DetailHeader'
 
-const LivePage = () => {
+const LivePage = async () => {
+  const data = await GetChat()
+  const user = { profile_img: 0, nick_name: '산타', user_id: 2 } // 추후 로그인 유저 정보 백엔드 연결 후 가져올 예정
   return (
-    <div className={styles.container}>
-      LivePage 입니다
-      <LiveStream />
-      <LiveChat />
+    <div className="inner-box">
+      <div className={styles.container}>
+        <DetailHeader title="라이브 스트리밍" />
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <h1>영상준비중입니다</h1>
+          <button className={styles.liveBtn}>재생하기</button>
+        </div>
+        <div className={styles.liveBox}>
+          <LiveStream />
+          <LiveChat serverData={data} user={user} />
+        </div>
+      </div>
     </div>
   )
 }
