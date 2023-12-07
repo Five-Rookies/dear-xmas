a17ef4c4139cdff5b8bd8e52af225339e63cfec4
junyeoplee <jyeop920@gmail.com>
2023-12-07 18:51:05 +0900

    add : supabase 회원가입,로그인,로그아웃 함수 생성
    ### 주요 기능
    supabase로 회원가입, 로그인, 로그아웃 처리 하는 함수 생성
    
    ### 진행 사항
    - [ ] 로그인 함수
    - [ ] 회원가입 함수
    - [ ] 로그아웃 함수
    
    ### 반영 브랜치
     feat/supabase -> develop
    
    close #108

--- src/app/(sign)/_components/SignUp.tsx
+++ src/app/(sign)/_components/SignUp.tsx
@@ -1,40 +1,83 @@
 'use client'
 
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import styles from '@/app/(sign)/_components/sign.module.scss'
+import { userSignUp } from '@/utils/apiRequest/signUserSupabase'
 
 const SignUp = () => {
+  const [nickName, setNickName] = useState<string>('')
+  const [userId, setUserId] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [checkPassword, setCheckPassword] = useState<string>('')
+  const [passwordHint, setPasswordHint] = useState<string>('')
+
+  const onSubmitSignUp = async (event: FormEvent) => {
+    event.preventDefault()
+
+    const option = {
+      user_name: nickName,
+      password_hint: passwordHint,
+    }
+
+    try {
+      await userSignUp(userId, password, option)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <main className={styles.container}>
       <h1>회원가입</h1>
-      <form>
+      <form onSubmit={onSubmitSignUp}>
         <div className="input-field">
           <h3>닉네임</h3>
           <input
             type="text"
             placeholder="특수문자 제외 8글자 이하로 입력해주세요"
+            onChange={e => {
+              setNickName(e.target.value)
+            }}
           />
         </div>
         <div className="input-field">
           <h3>아이디</h3>
-          <input type="text" placeholder="이메일을 입력해주세요" />
+          <input
+            type="text"
+            placeholder="이메일을 입력해주세요"
+            onChange={e => {
+              setUserId(e.target.value)
+            }}
+          />
         </div>
         <div className="input-field">
           <h3>비밀번호</h3>
           <input
-            type="text"
+            type="password"
             placeholder="영어,숫자,특수문자 포함 8~15 글자 입력해주세요"
+            onChange={e => {
+              setPassword(e.target.value)
+            }}
           />
         </div>
         <div className="input-field">
           <h3>비밀번호 확인</h3>
-          <input type="text" placeholder="비밀번호를 한번 더 입력해주세요" />
+          <input
+            type="password"
+            placeholder="비밀번호를 한번 더 입력해주세요"
+            onChange={e => {
+              setCheckPassword(e.target.value)
+            }}
+          />
         </div>
         <div className="input-field">
           <h3>비밀번호 찾기 힌트</h3>
           <input
             type="text"
             placeholder="크리스마스에 받았던 가장 좋은 선물은 무엇이었나요?"
+            onChange={e => {
+              setPasswordHint(e.target.value)
+            }}
           />
         </div>
 
---
+++ src/utils/apiRequest/signUserSupabase.ts
@@ -0,0 +1,54 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SERVICE_KEY as string,
+)
+
+export const userSignUp = async (
+  email: string,
+  password: string,
+  option: object,
+): Promise<void> => {
+  try {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: option,
+      },
+    })
+
+    if (error) return console.error(error)
+    console.log(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const userSignIn = async (
+  email: string,
+  password: string,
+): Promise<void> => {
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) return console.error(error)
+    console.log(data)
+    alert(data.user.email)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const userSignOut = async () => {
+  try {
+    const { error } = await supabase.auth.signOut()
+    if (error) return console.log(error)
+  } catch (error) {
+    console.log(error)
+  }
+}
