14c774b5a4505e408533bf9af6bafcb1708b3343
zxxng <jeayeong123@naver.com>
2023-12-12 15:54:18 +0900

    feat: 라우트 핸들러 로그인 기능 추가 #128

--- src/app/(sign)/_components/SignIn.tsx
+++ src/app/(sign)/_components/SignIn.tsx
@@ -1,56 +1,62 @@
 'use client'
 
-import React, { FormEvent, useState } from 'react'
+import React from 'react'
 import styles from '@/app/(sign)/_components/sign.module.scss'
 import Link from 'next/link'
-import { userSignIn } from '@/utils/apiRequest/signUserSupabase'
 import { useRouter } from 'next/navigation'
 
 const SignIn = () => {
-  const [userId, setUserId] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
   const router = useRouter()
 
-  const onSubmitUserSignIn = async (event: FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
     try {
-      await userSignIn(userId, password)
+      const formData = new FormData(event.currentTarget)
+      const response = await fetch('/auth/login', {
+        method: 'POST',
+        body: formData,
+      })
+
+      if (!response.ok) {
+        const errorData = await response.json()
+        throw new Error(errorData.error)
+      }
+
+      alert('로그인 완료. 반갑습니다!')
+      router.refresh()
       router.push('/')
     } catch (error) {
       alert(
-        '로그인 실패\n이메일이나 비밀번호가 잘못되었습니다. 다시 입력해주세요!',
+        '[ERROR]\n로그인 실패\n이메일이나 비밀번호가 잘못되었습니다. 다시 입력해주세요!',
       )
-      console.log(error)
+      console.error(error)
     }
   }
 
   return (
     <main className={styles.container}>
       <h1>로그인</h1>
-      <form onSubmit={onSubmitUserSignIn}>
+      <form onSubmit={handleSubmit}>
         <div className={styles.inputField}>
           <h3>이메일</h3>
           <input
+            name="email"
             type="email"
             placeholder="이메일을 입력해주세요"
-            onChange={e => {
-              setUserId(e.target.value)
-            }}
             required
           />
         </div>
         <div className={styles.inputField}>
           <h3>비밀번호</h3>
           <input
+            name="password"
             type="password"
             placeholder="비밀번호를 입력해주세요"
-            onChange={e => {
-              setPassword(e.target.value)
-            }}
             required
           />
         </div>
-        <button type="submit">로그인</button>
+        <button>로그인</button>
         <div className={styles.account}>
           <span>비밀번호를 잊으셨나요?</span>
           <Link href="signIn/findPassword">비밀번호 찾기</Link>
--- src/app/auth/login/route.ts
+++ src/app/auth/login/route.ts
@@ -7,35 +7,23 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request) {
   const requestUrl = new URL(request.url)
   const formData = await request.formData()
-  const userName = String(formData.get('user_name'))
   const email = String(formData.get('email'))
   const password = String(formData.get('password'))
-  const passwordHint = String(formData.get('password_hint'))
   const cookieStore = cookies()
   const supabase = createRouteHandlerClient({
     cookies: () => cookieStore,
   })
 
-  const option = {
-    user_name: userName,
-    password_hint: passwordHint,
-    profile_img: 0,
-  }
-
   try {
-    const { error } = await supabase.auth.signUp({
+    const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
-      options: {
-        data: option,
-        emailRedirectTo: `${requestUrl.origin}/auth/callback`,
-      },
     })
     if (error) throw error
   } catch (error) {
     console.error(error)
     return NextResponse.json(
-      { error: '[ERROR] 회원가입불가. supabase 통신 에러 발생' },
+      { error: '[ERROR] 로그인불가. supabase 통신 에러 발생' },
       { status: 500 },
     )
   }
