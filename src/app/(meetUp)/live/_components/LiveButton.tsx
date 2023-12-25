35b913873d6da3570e180677ee2085133a45680e
zxxng <jeayeong123@naver.com>
2023-12-25 16:26:10 +0900

    refactor: any 타입 정리 #213

--- src/app/(meetUp)/live/_components/LiveButton.tsx
+++ src/app/(meetUp)/live/_components/LiveButton.tsx
@@ -69,8 +69,11 @@ const LiveButton = ({ user_name, scheduling, meetup_id }: IMeetupBoardData) => {
     router.push('/meetup')
   }
 
-  const handleDeleteLive = async (e: any) => {
-    if (e.target.textContent === '종료하기') {
+  const handleDeleteLive = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    const target = e.target as HTMLButtonElement
+    if (target.textContent === '종료하기') {
       if (window.confirm('종료하시겠습니까?')) {
         setIsEnd(!isEnd)
         meetup_id && (await deleteLive(meetup_id))
@@ -88,7 +91,10 @@ const LiveButton = ({ user_name, scheduling, meetup_id }: IMeetupBoardData) => {
     </button>
   ) : (
     isNotEnded && (
-      <button onClick={handleDeleteLive} className={styles.liveDelayBtn}>
+      <button
+        onClick={e => handleDeleteLive(e)}
+        className={styles.liveDelayBtn}
+      >
         {`재생까지 ${dayRemaining} ${timeRemaining}`}
       </button>
     )
